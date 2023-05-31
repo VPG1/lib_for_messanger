60635ecdc9693b0fc224c5425161c0ceb1634fb1
Kirill <malihtorovich@mail.ru>
2023-05-31 22:43:45 +0300

    upgrade to new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lib_for_messanger",
-    version="0.4",
+    version="0.5",
     description="library for my messenger",
     url="https://github.com/VPG1/lib_for_messanger",
     author="Kirill Malihtorovich",
