e0abf9c7cbdff735af6f4e4c914da9dd032dad19
Kirill <malihtorovich@mail.ru>
2023-05-31 19:18:56 +0300

    fix message serialization

--- lib_for_messanger/message.py
+++ lib_for_messanger/message.py
@@ -18,14 +18,14 @@ class Message:
 
     def to_json_object(self):
         return {"sender_name": self.__sender_name,
-                "send_date": self.__send_date,
+                "send_date": self.__send_date.strftime("%Y-%m-%d %H:%M:%S"),
                 "message_text": self.__message_text}
 
     @staticmethod
     def from_json_object(json_object):
         message = Message("", "")
         message.__sender_name = json_object["sender_name"]
-        message.__send_date = json_object["send_date"]
+        message.__send_date = datetime.strptime(json_object["send_date"], "%Y-%m-%d %H:%M:%S")
         message.__message_text = json_object["message_text"]
 
         return message
